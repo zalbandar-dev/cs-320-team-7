e528e2ac481117243c332381cce11e6de552c43e
Aryan Patil <apatil.connect@gmail.com>
2026-04-05 19:20:25 -0400

    Added root package.json with concurrently dev script and gitignore in order to spin up backend and frontend servers concurrently

--- parkify/next.config.ts
+++ parkify/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+};
 
 export default nextConfig;
